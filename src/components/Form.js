46af48480e6819391dc74420a0e1165059685ad4
Leonardo Omaña <leotangram@gmail.com>
2019-06-16 20:24:24 -0500

    Consult api.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import axios from 'axios'
 
 function Form() {
+  const [criptocurrency, setCriptocurrency] = useState([])
+
+  useEffect(() => {
+    const consultAPI = async () => {
+      const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=15&tsym=USD`
+
+      const result = await axios.get(url)
+      
+      // Colocar respuesta en el state
+      setCriptocurrency(result.data.Data)
+    }
+
+    consultAPI()
+  }, [])
   return (
     <form>
       <div className='row'>
