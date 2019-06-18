c369c3199a9295dd28af819de1296a8de8fe9265
Leonardo Omaña <leotangram@gmail.com>
2019-06-18 14:25:50 -0500

    Show result.

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -5,6 +5,21 @@ const Quote = ({ result }) => {
   return (
     <div className="result">
       <h1>Resultado</h1>
+      <p className="price">
+        El precio es <span>{result.PRICE}</span>
+      </p>
+      <p>
+        Precio más alto del día: <span>{result.HIGHDAY}</span>
+      </p>
+      <p>
+        Precio más bajo del día: <span>{result.LOWDAY}</span>
+      </p>
+      <p>
+        Variación últimas 24 horas: <span>{result.CHANGEPCT24HOUR}</span>
+      </p>
+      <p>
+        Última actualización: <span>{result.LASTUPDATE}</span>
+      </p>
     </div>
   )
 }
